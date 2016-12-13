dbffddb3d18d7939d6d1ead944d7efd113c6518e
EnPing Hsieh <shieh.npin@gmail.com>
2016-12-14 00:47:00 +0800

    Add sharepreference impl

--- app/src/main/java/com/enping/codepath_persistence_sample/SharedPrefStorageImpl.java
+++ app/src/main/java/com/enping/codepath_persistence_sample/SharedPrefStorageImpl.java
@@ -4,32 +4,46 @@ import android.content.Context;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-/**
- * Created by Chjeng-Lun SHIEH on 2016/12/14.
- */
 public class SharedPrefStorageImpl implements PersistenceStorage {
 
-    public SharedPrefStorageImpl(Context baseContext) {
+    private final Context context;
 
+    public SharedPrefStorageImpl(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public List<String> getItems() {
+        Set<String> items = context
+                .getSharedPreferences("todoList", Context.MODE_PRIVATE)
+                .getStringSet("items", new HashSet<String>());
+        Log.d("Storage","Get: "+items);
+        return new ArrayList<>(items);
     }
 
     @Override
     public void addItem(String item) {
+        List<String> items = getItems();
+        items.add(item);
+        context.getSharedPreferences("todoList",Context.MODE_PRIVATE)
+                .edit()
+                .putStringSet("items",new HashSet<>(items))
+                .apply();
         Log.d("Storage","Add: "+item);
     }
 
     @Override
     public void removeItem(String item) {
+        List<String> items = getItems();
+        items.remove(item);
+        context.getSharedPreferences("todoList",Context.MODE_PRIVATE)
+                .edit()
+                .putStringSet("items",new HashSet<>(items))
+                .apply();
         Log.d("Storage","Remove: "+item);
-
-    }
-
-    @Override
-    public List<String> getItems() {
-
-        Log.d("Storage","Get ");
-        return new ArrayList<>();
     }
 }
