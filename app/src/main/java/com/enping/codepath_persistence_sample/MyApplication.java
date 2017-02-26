6efdfb933cdbf9b9617bd9c9c034fe93930d2286
En-Ping Hsieh <hsiehenping.fareastone@gmail.com>
2017-02-26 23:13:22 +0800

    Add stetho

--- app/src/main/java/com/enping/codepath_persistence_sample/MyApplication.java
+++ app/src/main/java/com/enping/codepath_persistence_sample/MyApplication.java
@@ -2,6 +2,7 @@ package com.enping.codepath_persistence_sample;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
 
@@ -13,6 +14,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Stetho.initializeWithDefaults(this);
         // This instantiates DBFlow
         FlowManager.init(new FlowConfig.Builder(this).build());
 
